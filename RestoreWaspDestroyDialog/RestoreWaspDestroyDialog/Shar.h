f8d8799c18530d2f56dfd03d7ccf1578174b0d9c
Proddy <josh@zhbot.org>
2025-04-11 00:51:50 +0100

    Update Shar.h
    Cleanup redundant header methods

--- RestoreWaspDestroyDialog/RestoreWaspDestroyDialog/Shar.h
+++ RestoreWaspDestroyDialog/RestoreWaspDestroyDialog/Shar.h
@@ -1,26 +1,7 @@
 #pragma once
 int IdentifySharVersion();
 
-void* GetMissionStageStart_Ptr();
-void* GetMissionInitialize_Ptr();
 void* GetFlyingActorReceiveEvent_Ptr();
-
-unsigned int GetCheats();
-void SetCheats(unsigned int enabledCheats);
-
-typedef void(_cdecl* SCRIPTCOMMANDPROC)(unsigned int uiCount, const char** prgpszArguments);
-void* GetScriptManager();
-bool AddScriptCommand(const char* pszName, const char* pszDescription, unsigned int uiMinimumArgumentCount, void* pScriptManager, SCRIPTCOMMANDPROC pCallback, unsigned int uiMaximumArgumentCount);
 
 void* GetEventManager();
 void TriggerEvent(void* pEventManager, unsigned int uiEvent, void* pUnknown);
-
-void* GetCharacterSheetManager();
-
-struct MissionScriptLoader
-{
-	unsigned char m_Offset[0x14];
-	void* Mission;
-	void* Stage;
-};
-MissionScriptLoader* GetMissionScriptLoader();
